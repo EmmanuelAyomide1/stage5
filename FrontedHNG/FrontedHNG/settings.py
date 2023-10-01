29fc4e995fe370c63de59a96b63f190b3184f078
EmmanuelAyomide1 <126819905+EmmanuelAyomide1@users.noreply.github.com>
2023-10-01 14:46:27 +0100

    Update settings.py

--- FrontedHNG/FrontedHNG/settings.py
+++ FrontedHNG/FrontedHNG/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-b!p%w*u@8b_!*r093%&u0x(1zuxw&fzssz%$)6u_$ptvn^^#@%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
